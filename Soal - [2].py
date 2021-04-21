345f3c3748ccbf87c5f7348a3e4b9606bf141b48
wibowodiditri <82685951+wibowodiditri@users.noreply.github.com>
2021-04-21 12:01:43 +0700

    Update Soal - [2].py

--- Soal - [2].py
+++ Soal - [2].py
@@ -3,7 +3,7 @@ txt = "{}".format (nama)
 print (txt)
 phi = 22/7
 hitung = int (input ("Masukkan jari - jari lingkaran :")) 
-print ("Luas lingkaran dengan jari-jari {} cm adalah {:.2f} cm{}" . format (hitung ,phi * hitung, "\u00b2"))
+print ("Luas lingkaran dengan jari-jari {} cm adalah {:.2f} cm{}" . format (hitung ,phi * hitung * hitung, "\u00b2"))
 
 
 
